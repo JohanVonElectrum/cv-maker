9ef7258085af637f368ba7e7cf912fec4343bf2c
JohanVonElectrum <joanestradium@gmail.com>
2025-02-19 09:57:27 +0100

    feat: deploy with gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: "/cv-maker",
     resolve: {
         alias: {
             "@": "/src",
